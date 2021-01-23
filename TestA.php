fb4b2252b4debb220149a3cc5bb2523913f532dd
Howard Chung <cryengine.studygroup@gmail.com>
2021-01-23 12:20:20 -0500

    add addition

---
+++ TestA.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+
+class TestA
+{
+    public static function add(int $num1, int $num2): int
+    {
+        return $num1 + $num2;
+    }
+}
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php declare(strict_types = 1);
+
+$sum = TestA::add(1,2);
+var_dump($sum);
