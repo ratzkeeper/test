cc39d7327d5e85522305bcb44491a8018a395a72
Howard Chung <cryengine.studygroup@gmail.com>
2021-01-23 11:55:19 -0500

    first init

---
+++ TestClass.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+
+class TestClass
+{
+    public static function add(int $num1, int $num2): int
+    {
+        return $num1 + $num2;
+    }
+}
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php declare(strict_types = 1);
+
+$sum = TestClass::add(1,2);
+var_dump($sum);
